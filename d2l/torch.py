677d95e2cb43dd2ab84d5f27eaa3840ceef65c6a
Mstislav <155465158+MstislavD@users.noreply.github.com>
2025-09-26 16:21:49 +0300

    implement HyperParameters class

--- d2l/torch.py
+++ d2l/torch.py
@@ -1,3 +1,4 @@
+import inspect
 from matplotlib import pyplot as plt
 from matplotlib_inline import backend_inline
 
@@ -44,3 +45,14 @@ def add_to_class(Class):
     def wrapper(obj):
         setattr(Class, obj.__name__, obj)
     return wrapper
+
+class HyperParameters:
+    """The base class of hyperparameters."""
+    def save_hyperparameters(self, ignore=[]):
+        """Save function arguments into class attributes."""
+        frame = inspect.currentframe().f_back
+        _, _, _, local_vars = inspect.getargvalues(frame)
+        self.hparams = {k:v for k, v in local_vars.items()
+                        if k not in set(ignore+['self']) and not k.startswith('_')}
+        for k, v in self.hparams.items():
+            setattr(self, k, v)
