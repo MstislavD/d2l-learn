493da399c886b0d0bbfb29d35049fd8d98a3cd12
Mstislav <155465158+MstislavD@users.noreply.github.com>
2025-09-29 15:09:07 +0300

    implement Module, DataModule, and Trainer classes

--- d2l/torch.py
+++ d2l/torch.py
@@ -1,6 +1,7 @@
 import inspect
 import collections
 from IPython import display
+from torch import nn
 from matplotlib import pyplot as plt
 from matplotlib_inline import backend_inline
 
@@ -47,6 +48,9 @@ def add_to_class(Class):
     def wrapper(obj):
         setattr(Class, obj.__name__, obj)
     return wrapper
+
+def cpu():
+    raise NotImplementedError
 
 class HyperParameters:
     """The base class of hyperparameters."""
@@ -105,3 +109,86 @@ class ProgressBoard(HyperParameters):
         axes.legend(plt_lines, labels)
         display.display(self.fig)
         display.clear_output(wait=True)
+        
+class Module(nn.Module, HyperParameters):
+    """The base class of models."""
+    def __init__(self, plot_train_per_epoch=2, plot_valid_per_epoch=1):
+        super().__init__()
+        self.save_hyperparameters()
+        self.board = ProgressBoard()
+    
+    def loss(self, y_hat, y):
+        raise NotImplementedError
+    
+    def forward(self, X):
+        assert hasattr(self, 'net'), 'Neural network is not defined'
+        return self.net(X)
+    
+    def plot(self, key, value, train):
+        """Plot a point in animation."""
+        assert hasattr(self, 'trainer'), "Trainer is not inited"
+        self.board.xlabel = 'epoch'
+        if train:
+            x = self.trainer.train_batch_idx / self.trainer.num_train_batches
+            n = self.trainer.num_train_batches / self.plot_train_per_epoch
+        else:
+            x = self.trainer.epoch + 1
+            n = self.trainer.num_val_batches / self.plot_valid_per_epoch
+        self.board.draw(x, value.to(cpu()).detach().numpy(),
+                        ('train_' if train else 'val_') + key,
+                        every_n=int(n))
+        
+    def training_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('loss', l, train=True)
+        
+    def validation_step(self, batch):
+        l = self.loss(self(*batch[:-1]), batch[-1])
+        self.plot('loss', l, train=False)
+        
+    def configure_optimizers(self):
+        raise NotImplementedError
+    
+class DataModule(HyperParameters):
+    """The base class of data."""
+    def __init__(self, root='../data', num_workers=4):
+        self.save_hyperparameters()
+        
+    def get_dataloader(self, train):
+        raise NotImplementedError
+    
+    def train_dataloader(self):
+        return self.get_dataloader(train=True)
+    
+    def val_dataloader(self):
+        return self.get_dataloader(train=False)
+    
+class Trainer(HyperParameters):
+    """The base class for training models with data."""
+    def __init__(self, max_epochs, num_gpus=0, gradient_clip_val=0):
+        self.save_hyperparameters()
+        assert num_gpus == 0, 'No GPU support yet'
+        
+    def prepare_data(self, data):
+        self.train_dataloader = data.train_dataloader()
+        self.val_dataloader = data.val_dataloader()
+        self.num_train_batches = len(self.train_dataloader)
+        self.num_val_batches = (len(self.val_dataloader) if self.val_dataloader is not None else 0)
+        
+    def prepare_model(self, model):
+        model.trainer = self
+        model.board.xlim = [0, self.max_epochs]
+        self.model = model
+        
+    def fit(self, model, data):
+        self.prepare_data(data)
+        self.prepare_model(model)
+        self.optim = model.configure_optimizers()
+        self.epoch = 0
+        self.train_batch_idx = 0
+        self.val_batch_idx = 0
+        for self.epoch in range(self.max_epochs):
+            self.fit_epoch()
+    
+    def fit_epoch(self):
+        raise NotImplementedError
