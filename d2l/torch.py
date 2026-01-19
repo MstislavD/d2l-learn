2737a7576d178f0ba63a6ff8b525a1a7f8691233
Mstislav <155465158+MstislavD@users.noreply.github.com>
2026-01-19 14:17:22 +0300

    implement The Base Classification Model (4.3)

--- d2l/torch.py
+++ d2l/torch.py
@@ -170,7 +170,7 @@ class Module(nn.Module, HyperParameters):
         self.plot('loss', l, train=False)
         
     def configure_optimizers(self):
-        raise NotImplementedError
+        torch.optim.SGD(self.parameters(), lr=self.lr)
     
 class DataModule(HyperParameters):
     """The base class of data."""
@@ -344,3 +344,16 @@ class FashionMNIST(DataModule):
             labels = self.text_labels(y)
         show_images(X.squeeze(1), nrows, ncols, titles=labels)
         
+class Classifier(Module):
+    """The base class of classification models."""
+    def validation_step(self, batch):
+        Y_hat = self(*batch[:-1])
+        self.plot('loss', self.loss(Y_hat, batch[-1]), train=False)
+        self.plot('acc', self.accuracy(Y_hat, batch[-1]), train=False)
+        
+    def accuracy(self, Y_hat, Y, averaged=True):
+        """Compute the number of correct predictions."""
+        Y_hat = Y_hat.reshape((-1, Y_hat.shape[-1]))
+        preds = Y_hat.argmax(axis=1).type(Y.dtype)
+        compare = (preds == Y.reshape(-1)).type(torch.float32)
+        return compare.mean() if averaged else compare
